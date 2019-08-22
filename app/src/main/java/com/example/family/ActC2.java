9a4c4cd287595d2a6e65decf15f8904f7c839416
yuqingmao <yuqing.mao@gmail.com>
2019-08-21 18:18:35 -0700

    Socket connection with server executable

--- app/src/main/java/com/example/family/ActC2.java
+++ app/src/main/java/com/example/family/ActC2.java
@@ -3,25 +3,67 @@ package com.example.family;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 
 
-public class ActC2 extends Activity {
-    public int weight = 0;
+public class ActC2 extends AppCompatActivity implements View.OnClickListener {
+    private Button bt1;
+    Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
+        mContext = this;
+        bt1 = findViewById(R.id.button2);
+        bt1.setOnClickListener(this);
 
         savefile();
         webviewPlay();
+    }
+
+    @Override
+    public void onClick(View view) {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    acceptServer();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+
+    private void acceptServer() throws IOException {
+        //1.创建客户端Socket，指定服务器地址和端口
+        Socket socket = new Socket("192.168.1.9", 12345);
+        //2.获取输出流，向服务器端发送信息
+        OutputStream os = socket.getOutputStream();//字节输出流
+        PrintWriter pw = new PrintWriter(os);//将输出流包装为打印流
+        //获取客户端的IP地址
+        InetAddress address = InetAddress.getLocalHost();
+        String ip = address.getHostAddress();
+        pw.write("客户端：~" + ip + "~ 接入服务器！！");
+        pw.flush();
+        socket.shutdownOutput();//关闭输出流
+        socket.close();
     }
 
     private void webviewPlay() {
